d3ef4a6fc8a3cf4575e43f341320f43d31017d1f
Win <windroid.note2@gmail.com>
2015-05-31 15:08:46 +0700

    Fix bugs

--- app/View/Helper/FlagHelper.php
+++ app/View/Helper/FlagHelper.php
@@ -6,25 +6,25 @@ class FlagHelper extends AppHelper {
         
     public function serialStatus($flag = NULL){
         //A=Available, R=Rent, S=Sold out, F=Fix(Repair)
-        $list = ['A'=>'Available', 'R'=>'Rent', 'F'=>'Repair', 'I'=>'Inactive'];
+        $list = array('A'=>'Available', 'R'=>'Rent', 'F'=>'Repair', 'I'=>'Inactive');
         return ($flag === NULL) ? $list : @$list[$flag];
     }
     
     public function forkliftGear($flag = NULL){
         //A=Auto, M=Manual
-        $list = ['A'=>'Auto', 'M'=>'Manual'];
+        $list = array('A'=>'Auto', 'M'=>'Manual');
         return ($flag === NULL) ? $list : @$list[$flag];
     }
     
     public function forkliftEngine($flag = NULL){
         //B=Battery, D=Diesel, G=Gasoline&LPG
-        $list = ['B'=>'Battery', 'D'=>'Diesel', 'G'=>'Gasoline&LPG'];
+        $list = array('B'=>'Battery', 'D'=>'Diesel', 'G'=>'Gasoline&LPG');
         return ($flag === NULL) ? $list : @$list[$flag];
     }
     
     public function contractStatus($flag = NULL){
         //A=Acitive, I=Inactive
-        $list = ['A'=>'Active', 'I'=>'Inactive'];
+        $list = array('A'=>'Active', 'I'=>'Inactive');
         return ($flag === NULL) ? $list : @$list[$flag];
     }
 }
